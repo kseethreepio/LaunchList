a337bc8e926bd302733652076f70a0de4f334f50
tachk <tachk@rocketeeringetc.net>
2015-04-11 18:06:02 -0700

    Buttons now working.

--- launchlist/views.py
+++ launchlist/views.py
@@ -2,6 +2,43 @@ from django.utils.safestring import mark_safe
 from django.template import Context, Template
 from django.http import HttpResponse
 
+buttonDict = {
+    'root': [["next", "/mission/"], ["back", None]],
+    'intro': [["next", "/mission/"], ["back", None]],
+    'mission': [["next", "/spacecraft/"], ["back", "/intro/"]],
+    'spacecraft': [["next", "/vehicle/"], ["back", "/mission/"]],
+    'vehicle': [["next", "/summary/"],["back", "/spacecraft"]],
+    'summary': [["next", None], ["back", "/vehicle/"]]
+}
+
+def create_nav_buttons(pageName):
+    # Make sure nothing left over from previous call
+    starting_page = False
+    ending_page = False
+    regular_page = True
+    
+    buttonsOnCurrentPage = buttonDict[pageName]
+
+    fButtonTemplate = open('launchlist/buttonTemplate.html')
+    tButtonView = Template(fButtonTemplate.read())
+    fButtonTemplate.close()
+    
+    # Get the URL for the 'back' button (first in the dict value entry)
+    if (buttonsOnCurrentPage[1][1] == None):
+        # If there's no 'back' button, create a 'start' button
+        starting_page = True        
+        html = tButtonView.render(Context({'starting_page': starting_page, 'nextURL': buttonsOnCurrentPage[0][1]}))
+    elif (buttonsOnCurrentPage[0][1] == None):
+        # If there's no 'next' page, create a back button and start over button
+        ending_page = True
+        html = tButtonView.render(Context({'ending_page': ending_page, 'backURL': buttonsOnCurrentPage[1][1]}))
+    else:
+        # Otherwise, create both a back and next button
+        regular_page = True        
+        html = tButtonView.render(Context({'regular_page': regular_page, 'backURL': buttonsOnCurrentPage[1][1], 'nextURL': buttonsOnCurrentPage[0][1]}))
+    
+    return html
+
 def view_root(request):
     fDefaultPageTemplate = open('launchlist/defaultPage.html')
     tCurrentView = Template(fDefaultPageTemplate.read())
@@ -10,7 +47,9 @@ def view_root(request):
     fMainContent = open('launchlist/content_intro.html')
     content_mainPage = mark_safe(fMainContent.read())
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    navButtons = create_nav_buttons("root")
+    
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
 
 def view_intro(request):
@@ -18,9 +57,12 @@ def view_intro(request):
     tCurrentView = Template(fDefaultPageTemplate.read())
     fDefaultPageTemplate.close()
     
-    content_mainPage = "This will be the intro page."
+    fMainContent = open('launchlist/content_intro.html')
+    content_mainPage = mark_safe(fMainContent.read())
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    navButtons = create_nav_buttons("intro")
+    
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
 
 def view_mission(request):
@@ -31,7 +73,9 @@ def view_mission(request):
     fMainContent = open('launchlist/content_mission.html')
     content_mainPage = mark_safe(fMainContent.read())
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    navButtons = create_nav_buttons("mission")
+    
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
 
 def view_spacecraft(request):
@@ -41,8 +85,10 @@ def view_spacecraft(request):
     
     fMainContent = open('launchlist/content_spacecraft.html')
     content_mainPage = mark_safe(fMainContent.read())
+
+    navButtons = create_nav_buttons("spacecraft")
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
 
 def view_launchVehicle(request):
@@ -52,8 +98,10 @@ def view_launchVehicle(request):
     
     fMainContent = open('launchlist/content_launchVehicle.html')
     content_mainPage = mark_safe(fMainContent.read())
+
+    navButtons = create_nav_buttons("vehicle")
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
 
 def view_summary(request):
@@ -64,5 +112,7 @@ def view_summary(request):
     fMainContent = open('launchlist/content_summary.html')
     content_mainPage = mark_safe(fMainContent.read())
     
-    html = tCurrentView.render(Context({'content_mainPage': content_mainPage}))
+    navButtons = create_nav_buttons("summary")
+    
+    html = tCurrentView.render(Context({'content_mainPage': content_mainPage, 'navButtons': navButtons}))
     return HttpResponse(html)
